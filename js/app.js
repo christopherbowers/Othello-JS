30e6cfdf70bc9d877826f0bf8a121a650f252d59
Christopher Bowers <c_bowers@me.com>
2021-11-19 11:36:34 -0500

    Replaced my gris init with a function to pull from the board array. Updated CSS slightly.

--- js/app.js
+++ js/app.js
@@ -1,49 +1,82 @@
+// Game board array
+// --------------------
+
 let board = [
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
+  ['', '', '',      '',      '', '', '', ''],
+  ['', '', '',      '',      '', '', '', ''],
+  ['', '', '',      '',      '', '', '', ''],
   ['', '', '', 'black', 'white', '', '', ''],
   ['', '', '', 'white', 'black', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', '']
+  ['', '', '',      '',      '', '', '', ''],
+  ['', '', '',      '',      '', '', '', ''],
+  ['', '', '',      '',      '', '', '', '']
 ];
 
-let blackScore = 0
-let whiteScore = 0
+
+// let blackScore
+// let whiteScore
+ 
 
 let player = 'black';
 let gameOver = false;
 
+
+// Render game board in HTML
+// -----------------------------
+
 const createBoard = () => {
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[row].length; col++) {
-      document.querySelector('.board').innerHTML += `<div id="${[row]}${[col]}" class="square"><div class=""></div></div>`;
+      document.querySelector('.board').innerHTML += `<div id="${[row]}${[col]}" class="square"><div class="">${[row]}${[col]}</div></div>`;
+//       document.querySelector('.board').innerHTML += `<div class="square"></div>`;
     }
   }
 }
 createBoard();
-let square = document.querySelectorAll('.square');
 
+// Place Pieces
+// ----------------------------------------
+let square = document.querySelectorAll('.square')
 
-const initGrid = () => {
-  document.getElementById(33).innerHTML = '<div class="">●</div>';
-  document.getElementById(33).classList.add('disabled');
-  document.getElementById(33).children[0].classList.add('black');
-  document.getElementById(44).innerHTML = '<div class="">●</div>';
-  document.getElementById(44).classList.add('disabled');
-  document.getElementById(44).children[0].classList.add('black');
+let placePieces = () => {
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    let position = board[row][col]
+    if (position === '') {
+    } else {
+      if (position === 'black') {
+        document.getElementById(`${[row]}${[col]}`).innerHTML = '<div class="black">●</div>';
+        document.getElementById(`${[row]}${[col]}`).classList.add('disabled');
+      }
+      if (position === 'white') {
+        document.getElementById(`${[row]}${[col]}`).innerHTML = '<div class="white">●</div>';
+        document.getElementById(`${[row]}${[col]}`).classList.add('disabled');
+      }
+    }
   
-  document.getElementById(34).innerHTML = '<div class="">●</div>';
-  document.getElementById(34).classList.add('disabled');
-  document.getElementById(34).children[0].classList.add('white');
-  document.getElementById(43).innerHTML = '<div class="">●</div>';
-  document.getElementById(43).classList.add('disabled');
-  document.getElementById(43).children[0].classList.add('white');
+  }
+  }
 }
-initGrid()
+placePieces();
 
+// const initGrid = () => {
+//   document.getElementById(33).innerHTML = '<div class="">●</div>';
+//   document.getElementById(33).classList.add('disabled');
+//   document.getElementById(33).children[0].classList.add('black');
 
+//   document.getElementById(44).innerHTML = '<div class="">●</div>';
+//   document.getElementById(44).classList.add('disabled');
+//   document.getElementById(44).children[0].classList.add('black');
+//   
+//   document.getElementById(34).innerHTML = '<div class="">●</div>';
+//   document.getElementById(34).classList.add('disabled');
+//   document.getElementById(34).children[0].classList.add('white');
+
+//   document.getElementById(43).innerHTML = '<div class="">●</div>';
+//   document.getElementById(43).classList.add('disabled');
+//   document.getElementById(43).children[0].classList.add('white');
+// }
+// initGrid()
 
 
 
@@ -53,34 +86,36 @@ initGrid()
 // Switch between black's and white's turns
 // ----------------------------------------
 
-const toggleTurn = () => {
-  if (player === 'black') {
-    player = 'white';
-  } else if (player === 'white') {
-    player = 'black'
-    }
-}
+// const toggleTurn = () => {
+//   if (player === 'black') {
+//     player = 'white';
+//   } else if (player === 'white') {
+//     player = 'black'
+//     }
+// }
 
 
-const updateScore = () => {
-  let blackScore = board.filter(x => x === "black").length;
-  let whiteScore = board.filter(x => x === "white").length;
-}
-
-// Game play
-// ----------------------------------------
 
 
-for (let i = 0; i < square.length; i++) {
-    square[i].addEventListener('click', () => {
-      if (square[i].innerText != '●' && !gameOver){
-        square[i].innerText = `●`;
-        square[i].classList.add('disabled');
-        square[i].classList.add(`${player}`);
-        board[i] = player;
 
-        toggleTurn();
-        updateScore();
-      }
-      });
-};
+
+
+
+
+
+// for (let i = 0; i < square.length; i++) {
+//     square[i].addEventListener('click', () => {
+//       if (square[i].innerText != '●' && !gameOver){
+//         square[i].innerText = `●`;
+//         square[i].classList.add('disabled');
+//         square[i].classList.add(`${player}`);
+//         board[i] = player;
+//        
+//         toggleTurn();
+//        
+//        
+// //     console.log(blackScore)
+//       }
+//       });
+// };
+
